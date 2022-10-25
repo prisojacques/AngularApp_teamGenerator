1c9aefb5deed1da36ffe2b3d55f5eb9082338be6
Jacques Priso <jacques.jackeve.priso@mni.thm.de>
2022-10-25 17:11:23 +0200

    use of Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LogikService} from "./logik.service";
 
 @Component({
   selector: 'app-root',
@@ -6,58 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- newMemberName= '';
- members: string[] = [];
- errorMessage = '';
- numberOfTeams: number | "" = "";
- teams: string[] [] =[];
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | "" = "";
+  teams: string[] [] = [];
 
- onInput(member:string){
-   this.newMemberName = member;
- }
+  constructor(private logikService: LogikService) {
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
 
   addMember() {
-   if(!this.newMemberName){
-     this.errorMessage = "Name cannot be empty!!";
-     return;
-   }
-   this.errorMessage='';
-   this.members.push(this.newMemberName);
-   this.newMemberName='';
+    if (!this.newMemberName) {
+      this.errorMessage = "Name cannot be empty!!";
+      return;
+    }
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
   }
 
   onNumberOfTeamsInput(value: string) {
     this.numberOfTeams = Number(value);
   }
 
-  generateTeams(){
-    if(!this.numberOfTeams || this.numberOfTeams <= 0){
-      this.errorMessage = 'Invalid number of Team';
-      return;
-    }
-
-    if(this.members.length < this.numberOfTeams){
-      this.errorMessage = 'Not enough memebers';
-      return;
-    }
-    const allMembers = [...this.members];
-
-    this.errorMessage= '';
-
-    while (allMembers.length){
-      for(let i= 0; i<this.numberOfTeams; i++){
-        const randomIndex = Math.floor(Math.random() * allMembers.length);
-        const member = allMembers.splice(randomIndex, 1)[0];
-
-        if(!member) break;
-
-        if(this.teams[i]){
-          this.teams[i].push(member);
-        } else {
-          this.teams[i] = [member];
-        }
-      }
-    }
-
+  generateTeams() {
+    this.logikService.generateTeams(this.numberOfTeams, this.errorMessage, this.members, this.teams);
   }
 }
